813f8ab9325fbbe0017e1b4796370c43dc52bbc9
HermersonDev <ne.hermerson@gmail.com>
2020-07-10 22:20:28 -0300

    :construction: call action signInRequest into Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {} from '@reduxjs/toolkit';
+
+import { signInRequest } from '../../redux/auth/actions';
 
 import ifrnLogo from '../../assets/images/ifrn-logo-main.svg';
 import banner from '../../assets/images/banner.svg';
 
 import { Container, Left } from './styles';
 
-const Login = () => (
-  <Container>
-    <Left>
-      <img className="logo" src={ifrnLogo} alt="ifleads" />
-      <form>
-        <label htmlFor="email">
-          E-mail
-          <input id="email" type="email" placeholder="Digite seu e-mail" />
-        </label>
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassowrd] = useState('');
+  const dispatch = useDispatch();
 
-        <label htmlFor="password">
-          Senha
-          <input id="password" type="password" placeholder="Digite sua senha" />
-        </label>
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log('submit');
+    dispatch(signInRequest({ email, password }));
+  }
 
-        <button type="submit">entrar</button>
-      </form>
-    </Left>
+  return (
+    <Container>
+      <Left>
+        <img className="logo" src={ifrnLogo} alt="ifleads" />
+        <form onSubmit={submitHandler}>
+          <label htmlFor="email">
+            E-mail
+            <input
+              id="email"
+              type="email"
+              placeholder="Digite seu e-mail"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </label>
 
-    <img className="main-image" src={banner} alt="things" />
-  </Container>
-);
+          <label htmlFor="password">
+            Senha
+            <input
+              id="password"
+              type="password"
+              placeholder="Digite sua senha"
+              value={password}
+              onChange={(e) => {
+                setPassowrd(e.target.value);
+              }}
+            />
+          </label>
+
+          <button type="submit">entrar</button>
+        </form>
+      </Left>
+
+      <img className="main-image" src={banner} alt="things" />
+    </Container>
+  );
+};
 
 export default Login;
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route as R, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -6,14 +7,14 @@ import Default from '../layouts/Default';
 import Auth from '../layouts/Auth';
 
 function Route({ auth, component: Component, ...restOfProps }) {
-  const signed = false;
+  const signed = useSelector((state) => state.auth.signed);
 
   if (signed && !auth) {
-    return <Redirect to="/" />;
+    return <Redirect to="/materials" />;
   }
 
   if (!signed && auth) {
-    return <Redirect to="/login" />;
+    return <Redirect to="/" />;
   }
 
   const Layout = auth ? Auth : Default;
